99594cbae84d794d1ceec447fbc57adbc54e458c
Jaakko <panula.jaakko@gmail.com>
2019-01-21 11:43:03 +0200

    Lisätty extensionmethods skripti ja tehty getoraddcomponent-metodi

---
+++ ProjectSpaceIndy/Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods {
+
+    public static TComponent GetOrAddComponent<TComponent>(this GameObject gameObject)
+        where TComponent : Component
+    {
+        TComponent component = gameObject.GetComponent<TComponent>();
+        if (component == null)
+        {
+            component = gameObject.AddComponent<TComponent>();
+        }
+
+        return component;
+    }
+}
