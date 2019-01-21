c364f10cca2bf5ec957e3e50bcdd980830fe9625
Jaakko <panula.jaakko@gmail.com>
2019-01-21 13:53:47 +0200

    Unitbase tehty

---
+++ ProjectSpaceIndy/Assets/Scripts/UnitBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UnitBase : MonoBehaviour, IDamageReceiver
+{
+
+	public IMover Mover;
+	public IHealth Health;
+	public WeaponBase[] Weapons;
+
+	protected virtual void Awake()
+	{
+		Health = gameObject.GetOrAddComponent<Health>();
+	}
+
+	protected abstract void Update();
+
+	public virtual bool TakeDamage(int amount)
+	{
+		bool died = Health.DecreaseHealth(amount);
+		if (died)
+		{
+			Die();
+		}
+
+		return died;
+	}
+
+	public void Fire()
+	{
+		foreach (WeaponBase weapon in Weapons)
+		{
+			weapon.Fire();
+		}
+	}
+
+	protected virtual void Die()
+	{
+		Destroy(gameObject);
+	}
+}
