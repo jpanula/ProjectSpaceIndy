14b53bc25781589fc8b6bf8ca61d86e9b73f6b2f
Jaakko <panula.jaakko@gmail.com>
2019-01-21 11:18:47 +0200

    Health interface tehty

---
+++ ProjectSpaceIndy/Assets/Scripts/Interfaces/IHealth.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHealth {
+    /// <summary>
+    /// Returns the current amount of health points
+    /// </summary>
+    int CurrentHealth { get; }
+    
+    /// <summary>
+    /// Returns the maximum amount of health points
+    /// </summary>
+    int MaxHealth { get; }
+    
+    /// <summary>
+    /// Return the minimum amount of health points
+    /// </summary>
+    int MinHealth { get; }
+    
+    /// <summary>
+    /// Tells whether the unit currently cannot die. The unit can still take damage
+    /// </summary>
+    bool isImmortal { get; set; }
+    
+    /// <summary>
+    /// Tells whether the unit currently cannot take any damage
+    /// </summary>
+    bool isInvunerable { get; set; }
+    
+    /// <summary>
+    /// Increases current health points by the given amount. Will not increase beyond maximum health
+    /// </summary>
+    /// <param name="amount">Amount of health points to add</param>
+    void IncreaseHealth(int amount);
+    
+    /// <summary>
+    /// Decreases current health points by the given amount. Will not decrease under minimum health
+    /// </summary>
+    /// <param name="amount">Amount of health points to subtract</param>
+    /// <returns>True if the unit dies (current health reaches minimum health). Else false</returns>
+    bool DecreaseHealth(int amount);
+    
+    /// <summary>
+    /// Resets the health
+    /// </summary>
+    void Reset();
+}
