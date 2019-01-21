597e73239dc252a4c25c84521c490f3058896d11
Jaakko <panula.jaakko@gmail.com>
2019-01-21 11:24:03 +0200

    Mover interface tehty

---
+++ ProjectSpaceIndy/Assets/Scripts/Interfaces/IMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMover{
+    /// <summary>
+    /// Speed of movement
+    /// </summary>
+    float Speed { get; }
+    
+    /// <summary>
+    /// Method for moving
+    /// </summary>
+    /// <param name="movementVector">The movement vector</param>
+    void Move(Vector3 movementVector);
+}
