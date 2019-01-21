30646481b2905ef7727e2cf05147b61e84dde945
Jaakko <panula.jaakko@gmail.com>
2019-01-21 11:31:03 +0200

    DamageReceiver interface tehty

---
+++ ProjectSpaceIndy/Assets/Scripts/Interfaces/IDamageReceiver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageReceiver {
+    /// <summary>
+    /// Takes damage according to the amount
+    /// </summary>
+    /// <param name="amount">The amount of damage to take</param>
+    /// <returns>True if the unit dies, false otherwise</returns>
+    bool TakeDamage(int amount);
+    
+}
