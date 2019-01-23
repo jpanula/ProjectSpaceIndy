c6090a8f95653ccb083eea6242e1b63d6315a72a
Jaakko <panula.jaakko@gmail.com>
2019-01-23 12:41:02 +0200

    projectile pool skripti tehty

---
+++ ProjectSpaceIndy/Assets/Scripts/ProjectilePool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectilePool : GenericPool<Projectile> {
+    
+    protected override void Activate(Projectile item)
+    {
+        base.Activate(item);
+        item.gameObject.SetActive(true);
+    }
+
+    protected override void Deactivate(Projectile item)
+    {
+        base.Deactivate(item);
+        item.gameObject.SetActive(false);
+    }
+}
