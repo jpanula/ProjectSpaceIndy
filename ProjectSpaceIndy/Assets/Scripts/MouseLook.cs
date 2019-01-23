c87b3c47b254952d9cc3504a9cb78d3ba39a1468
Jaakko <panula.jaakko@gmail.com>
2019-01-23 15:48:10 +0200

    mouselook skripti lisätty (kääntää objektin katsomaan hiirtä kohti)

---
+++ ProjectSpaceIndy/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Camera Camera;
+    private Plane _plane;
+    private float _distanceToPlane;
+    private Vector3 _pointOnPlane;
+
+    private void Awake()
+    {
+        _plane = new Plane(Vector3.up, 0);
+    }
+
+    private void Update()
+    {
+        Ray mouseRay = Camera.ScreenPointToRay(Input.mousePosition);
+        if (_plane.Raycast(mouseRay, out _distanceToPlane))
+        {
+            _pointOnPlane = mouseRay.GetPoint(_distanceToPlane);
+            Debug.DrawLine(_pointOnPlane + new Vector3(0, -2, 0), _pointOnPlane + new Vector3(0, 2, 0), Color.green, 0.5f);
+            transform.LookAt(new Vector3(_pointOnPlane.x, transform.position.y, _pointOnPlane.z));
+        }
+    }
+}
