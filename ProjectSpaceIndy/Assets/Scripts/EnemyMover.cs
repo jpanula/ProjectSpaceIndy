49d2ca4b20874e8dcef16fb946ba41e01d31f454
Jaakko <panula.jaakko@gmail.com>
2019-01-23 18:00:13 +0200

    tehty vihu-unitille jonkunnäkönen liikkuminen placeholderiks

---
+++ ProjectSpaceIndy/Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour, IMover
+{
+    public float _speed;
+    public float TurnSmoothing;
+    public float MoveSmoothing;
+    public GameObject Player;
+
+    private void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+
+    public void Move(Vector3 movementVector)
+    {
+        Quaternion lookRotation = Quaternion.LookRotation(Player.transform.position - transform.position, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, 1 / TurnSmoothing * Time.deltaTime);
+
+        transform.position =
+            Vector3.Lerp(transform.position, transform.position + movementVector * _speed, 1 / MoveSmoothing * Time.deltaTime);
+    }
+}
---
+++ ProjectSpaceIndy/Assets/Scripts/EnemyUnit.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyUnit : UnitBase
+{
+    public GameObject Player;
+    public float StrafeTime;
+    public float WaitTime;
+    public float Distance;
+    public bool MoveLeft;
+    public bool Waiting;
+    private float _strafeTimer;
+    private float _waitTimer;
+    private Vector3 _movementVector;
+
+    private void Start()
+    {
+        _strafeTimer = 0;
+        _waitTimer = 0;
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    protected override void Update()
+    {
+        _movementVector = Vector3.zero;
+        if (Waiting)
+        {
+            _waitTimer += Time.deltaTime;
+            Mover.Move(_movementVector * Time.deltaTime);
+            if (_waitTimer >= WaitTime)
+            {
+                Waiting = false;
+                _waitTimer = 0;
+            }
+        }
+        else
+        {
+            _strafeTimer += Time.deltaTime;
+            if (MoveLeft)
+            {
+                _movementVector = -transform.right;
+            }
+            else
+            {
+                _movementVector = transform.right;
+            }
+
+            if (_strafeTimer >= StrafeTime)
+            {
+                Waiting = true;
+                _strafeTimer = 0;
+                MoveLeft = !MoveLeft;
+            }
+
+            Vector3 playerVector = Vector3.Normalize(Player.transform.position - transform.position);
+            float distanceFromPlayer = Vector3.Distance(Player.transform.position, transform.position);
+            playerVector *= (distanceFromPlayer - Distance) / Distance;
+            Mover.Move((_movementVector + playerVector) * Time.deltaTime);
+        }
+    }
+}
