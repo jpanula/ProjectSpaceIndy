72e18c169a07234cf19a35c4665d72511b1b592a
Jaakko <panula.jaakko@gmail.com>
2019-01-23 14:03:13 +0200

    formatting

--- ProjectSpaceIndy/Assets/Scripts/WeaponTester.cs
+++ ProjectSpaceIndy/Assets/Scripts/WeaponTester.cs
@@ -7,12 +7,6 @@ public class WeaponTester : MonoBehaviour
 
 	public Weapon[] Weapons;
 	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		if (Input.GetAxisRaw("Fire1") > 0)
 		{
