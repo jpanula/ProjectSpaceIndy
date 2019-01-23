92ab1e95e7896f89a3feb7bede9a42c82ce6f1d3
Jaakko <panula.jaakko@gmail.com>
2019-01-23 13:42:59 +0200

    Testiskriptejä

---
+++ ProjectSpaceIndy/Assets/Scripts/TestProjectileMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestProjectileMover : MonoBehaviour, IMover
+{
+
+    public float _speed;
+
+    public float Speed
+    {
+        get
+        {
+            return _speed;
+        }
+    }
+
+    public void Move(Vector3 movementVector)
+    {
+        transform.Translate(movementVector * Speed);
+    }
+}
---
+++ ProjectSpaceIndy/Assets/Scripts/WeaponTester.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponTester : MonoBehaviour
+{
+
+	public Weapon[] Weapons;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			foreach (Weapon weapon in Weapons)
+			{
+				weapon.Fire();
+			}
+		}
+	}
+}
