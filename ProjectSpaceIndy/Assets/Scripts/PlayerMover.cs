2e3c397c9a34d8e0b721976cd5f12dca65b8339c
Jaakko <panula.jaakko@gmail.com>
2019-01-23 16:01:05 +0200

    playermoveriin lisätty kääntyminen hiiren perässä

--- ProjectSpaceIndy/Assets/Scripts/PlayerMover.cs
+++ ProjectSpaceIndy/Assets/Scripts/PlayerMover.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour, IMover
 {
 
+	[Tooltip("Camera from which to take rotation when using the mouse")]
+	public Camera Camera;
 	public float _speed = 1;
 	[Range(0.1f, 1.0f), Tooltip("Bigger number = Ship turns slower")]
 	public float turnSmoothing;
 	private float rotationSpeed;
-	
+	private Plane _plane;
+	private float _distanceToPlane;
+	private Vector3 _pointOnPlane;
+
+	private void Awake()
+	{
+		_plane = new Plane(Vector3.up, 0);
+	}
 
 	public float Speed
 	{
@@ -25,7 +34,17 @@ public class PlayerMover : MonoBehaviour, IMover
 		position = position + movementVector * _speed;
 		Vector3 lookAt = position - transform.position;
 		rotationSpeed = 1 / turnSmoothing;
-		if (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
+		
+		// Get rotation from mouse if possible
+		Ray mouseRay = Camera.ScreenPointToRay(Input.mousePosition);
+		if (_plane.Raycast(mouseRay, out _distanceToPlane))
+		{
+			//Debug.DrawLine(_pointOnPlane + new Vector3(0, -2, 0), _pointOnPlane + new Vector3(0, 2, 0), Color.green, 0.5f);
+			_pointOnPlane = mouseRay.GetPoint(_distanceToPlane);
+			transform.LookAt(new Vector3(_pointOnPlane.x, transform.position.y, _pointOnPlane.z));
+		}
+		// If mouse rotation fails, use movement
+		else if (Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
 		{
 			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookAt),
 				rotationSpeed * Time.deltaTime);
