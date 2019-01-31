23f34c4f8f1889cd78afdd94ee4db4d41e686beb
Leia <laaksonenleia@gmail.com>
2019-01-31 16:42:58 +0200

    Viholliselle lisätty ampuminen
    Ampuu X ajan välein

--- ProjectSpaceIndy/Assets/Scripts/EnemyUnit.cs
+++ ProjectSpaceIndy/Assets/Scripts/EnemyUnit.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class EnemyUnit : UnitBase
 {
+    public float CoolDownTime;
+    private float _timer;
+    
     protected override void Update()
     {
-        
+        _timer += Time.deltaTime;
+
+        if (_timer >= CoolDownTime)
+        {
+            foreach (Weapon weapon in Weapons)
+            {
+                weapon.Fire();
+            }
+
+            _timer = 0;
+        }
     }
 }
