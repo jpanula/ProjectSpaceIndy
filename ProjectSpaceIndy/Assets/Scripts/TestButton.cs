4cdb50e7289248ff367fee5154fbcebc5709ed94
Leia <laaksonenleia@gmail.com>
2019-02-06 16:53:14 +0200

    An attempt was made to abstract classes
    < _ >

---
+++ ProjectSpaceIndy/Assets/Scripts/TestButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButton : MonoBehaviour, IActivator
+{
+    private bool _active;
+
+    public bool Active
+    {
+        get { return _active; }
+    }
+}
