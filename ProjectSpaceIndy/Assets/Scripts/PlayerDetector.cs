d3f37877dd078c2c9b41cd4944758c91c33148bd
Jaakko <panula.jaakko@gmail.com>
2019-02-07 16:51:00 +0200

    started working on playerdetector

---
+++ ProjectSpaceIndy/Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : ActivatorBase
+{
+    private bool _active;
+
+    public override bool Active
+    {
+        get { return _active; }
+    }
+
+    
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position + GetComponent<BoxCollider>().center, Vector3.Scale(transform.localScale, GetComponent<BoxCollider>().size));
+    }
+}
