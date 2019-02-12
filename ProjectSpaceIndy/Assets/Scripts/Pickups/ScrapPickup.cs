b6a28a3646990c83c90a7046ae048984c6dc0849
Jaakko <panula.jaakko@gmail.com>
2019-02-12 16:47:55 +0200

    started work on scrap pickup

---
+++ ProjectSpaceIndy/Assets/Scripts/Pickups/ScrapPickup.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrapPickup : PickupBase
+{
+    protected override void GrantEffect(PlayerUnit playerUnit)
+    {
+        throw new System.NotImplementedException();
+    }
+}
