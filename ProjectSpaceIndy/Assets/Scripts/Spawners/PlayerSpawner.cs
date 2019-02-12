2e8d42c977d8123e73b8c0090fdbc0f7965a6c36
Jaakko <panula.jaakko@gmail.com>
2019-02-12 16:47:25 +0200

    made playerspawner
    even though it's not in use right now or even good

---
+++ ProjectSpaceIndy/Assets/Scripts/Spawners/PlayerSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : Spawner<PlayerUnit>
+{
+    public Camera MainCamera;
+    private PlayerUnit _player;
+
+    private void Update()
+    {
+        if (_player == null)
+        {
+            _player = Spawn();
+            MainCamera.GetComponent<FollowCamera>().target = _player.gameObject;
+            _player.GetComponent<PlayerMover>().Camera = MainCamera;
+        }
+    }
+
+    protected override void PostSpawn(PlayerUnit spawnedObject)
+    {
+        MainCamera.GetComponent<FollowCamera>().target = spawnedObject.gameObject;
+        spawnedObject.GetComponent<PlayerMover>().Camera = MainCamera;
+    }
+}
