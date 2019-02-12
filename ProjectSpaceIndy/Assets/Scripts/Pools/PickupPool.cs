06a7bda5321409b56c2ab49493bdab13ad9d4fe8
Jaakko <panula.jaakko@gmail.com>
2019-02-12 16:18:28 +0200

    made Pickup pool

---
+++ ProjectSpaceIndy/Assets/Scripts/Pools/PickupPool.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupPool : GenericPool<PickupBase>
+{
+        
+    protected override void Activate(PickupBase item)
+    {
+        base.Activate(item);
+        item.gameObject.SetActive(true);
+    }
+
+    protected override void Deactivate(PickupBase item)
+    {
+        base.Deactivate(item);
+        item.gameObject.SetActive(false);
+    }
+}
