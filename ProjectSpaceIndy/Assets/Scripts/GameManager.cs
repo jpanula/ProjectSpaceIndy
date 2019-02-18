a4d10706e3aa1974388fb447cfaf5d8320f0bde8
Jaakko <panula.jaakko@gmail.com>
2019-02-18 18:45:43 +0200

    forgot to commit script from gamemanager

---
+++ ProjectSpaceIndy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public static int Score;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
