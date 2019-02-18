8dbe410a3d2f1aa2f2f7b1e211c97be42811fcbd
Jaakko <panula.jaakko@gmail.com>
2019-02-18 19:31:46 +0200

    added health bar for player

--- ProjectSpaceIndy/Assets/Scripts/Units/PlayerUnit.cs
+++ ProjectSpaceIndy/Assets/Scripts/Units/PlayerUnit.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUnit : UnitBase
 {
 
+	public Image HealthBar;
+	public float HealthBarChangeSpeed;
 	private Vector3 _spawnPosition;
 	
 	protected override void Update ()
 	{
+		float fillAmount = (float) Health.CurrentHealth / Health.MaxHealth;
+		HealthBar.fillAmount = Mathf.Lerp(HealthBar.fillAmount, fillAmount, Time.deltaTime * HealthBarChangeSpeed);
 	}
 
 	private void OnEnable()
