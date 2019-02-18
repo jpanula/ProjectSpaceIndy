11b2bab83500f6bc0ccb88ed4502db0837ac9688
Jaakko <panula.jaakko@gmail.com>
2019-02-18 18:46:00 +0200

    added score increase to scrap pickup

--- ProjectSpaceIndy/Assets/Scripts/Pickups/ScrapPickup.cs
+++ ProjectSpaceIndy/Assets/Scripts/Pickups/ScrapPickup.cs
@@ -41,7 +41,7 @@ public class ScrapPickup : PickupBase
     
     protected override void GrantEffect(PlayerUnit playerUnit)
     {
-        //TODO add score system
+        GameManager.Score += 1;
     }
 
     protected override void OnTriggerEnter(Collider other)
