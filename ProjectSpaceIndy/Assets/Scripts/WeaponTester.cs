d37887179ae0d8129c03de95dfdae7c86974d5b2
Jaakko <panula.jaakko@gmail.com>
2019-02-18 12:39:09 +0200

    made projectiles not derp and added gizmo to show projectile hitbox

--- ProjectSpaceIndy/Assets/Scripts/WeaponTester.cs
+++ ProjectSpaceIndy/Assets/Scripts/WeaponTester.cs
@@ -16,7 +16,7 @@ public class WeaponTester : MonoBehaviour
 		
 		Vector3 rightStickVector = new Vector3(Input.GetAxisRaw("Horizontal_Look"), 0, Input.GetAxisRaw("Vertical_Look"));
 		float rightStickMagnitude = Vector3.Magnitude(rightStickVector);
-		if (Input.GetAxisRaw("Fire1") > 0 || (rightStickMagnitude >= rightStickDeadzone && !useMouse))
+		if (Input.GetAxisRaw("Fire1") > 0 || rightStickMagnitude >= rightStickDeadzone && !useMouse)
 		{
 			foreach (Weapon weapon in Weapons)
 			{
