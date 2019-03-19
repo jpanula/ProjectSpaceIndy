ad28194eb6af14495c22538692c8fa193ec80470
Jaakko <panula.jaakko@gmail.com>
2019-03-19 14:19:11 +0200

    added spinning enemy

--- ProjectSpaceIndy/Assets/Scripts/Movers/BasicEnemyMover.cs
+++ ProjectSpaceIndy/Assets/Scripts/Movers/BasicEnemyMover.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class BasicEnemyMover : MonoBehaviour, IMover
 {
+    
+    public float TurnSpeed;
+    public float _speed;
+    public ForwardVector Forward = ForwardVector.Forward;
+    private LayerMask _layerMask = (int) (Const.Layers.Player | Const.Layers.Activator | Const.Layers.Environment | Const.Layers.Enemy);
+    private SphereCollider _collider;
+
+    public enum ForwardVector
+    {
+        Forward,
+        MovementVector
+    }
+    
     public float Speed
     {
         get { return _speed; }
         set { _speed = value; }
     }
-
     public Vector3 MovementVector { get; set; }
-    public float TurnSpeed;
-
-    public float _speed;
-    public void ResetMover()
+    
+    private void Awake()
     {
-        Speed = 0;
-        MovementVector = Vector3.zero;
+        _collider = GetComponent<SphereCollider>();
     }
 
     private void Update()
@@ -28,7 +37,46 @@ public class BasicEnemyMover : MonoBehaviour, IMover
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * TurnSpeed);
         }
 
-        Vector3 newPos = transform.position + transform.forward * Speed;
-        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime);
+        var t = transform;
+        var position = t.position;
+        Vector3 newPos;
+        switch (Forward)
+        {
+            case ForwardVector.Forward:
+                newPos = position + t.forward * Speed;
+                break;
+            case ForwardVector.MovementVector:
+                newPos = position + MovementVector.normalized * Speed;
+                break;
+            default:
+                newPos = position + t.forward * Speed;
+                break;
+        }
+        
+        newPos = Vector3.Lerp(position, newPos, Time.deltaTime);
+        Vector3 direction = newPos - position;
+
+        RaycastHit hit;
+        if (Physics.SphereCast(position, _collider.radius, direction, out hit,
+            Vector3.Distance(position, newPos), _layerMask))
+        {
+            direction = Vector3.ProjectOnPlane(direction, hit.normal);
+            newPos = position + direction;
+
+            if (Physics.SphereCast(position, _collider.radius, direction, out hit,
+                Vector3.Distance(position, newPos), _layerMask))
+            {
+                direction = Vector3.ProjectOnPlane(direction, hit.normal);
+                newPos = position + direction;
+            }
+        }
+        
+        transform.position = newPos;
+    }
+    
+    public void ResetMover()
+    {
+        Speed = 0;
+        MovementVector = Vector3.zero;
     }
 }
---
+++ ProjectSpaceIndy/Assets/Scripts/Units/PartDestroyer.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartDestroyer : MonoBehaviour
+{
+    public DestructionMethods DestructionMethod;
+    public int[] Tresholds;
+    public GameObject[] GameObjects;
+    public ParticleSystem DestroyEffect;
+    private Dictionary<int, GameObject> _values;
+    private Health _health;
+
+    public enum DestructionMethods
+    {
+        Disable,
+        Destroy
+    }
+
+    private void Awake()
+    {
+        if (Tresholds.Length < GameObjects.Length)
+        {
+            GameObject[] newObjects = new GameObject[Tresholds.Length];
+            for (int i = 0; i < Tresholds.Length; i++)
+            {
+                newObjects[i] = GameObjects[i];
+            }
+
+            GameObjects = newObjects;
+        }
+        else if (GameObjects.Length < Tresholds.Length)
+        {
+            int[] newTresholds = new int[GameObjects.Length];
+            for (int i = 0; i < GameObjects.Length; i++)
+            {
+                newTresholds[i] = Tresholds[i];
+            }
+
+            Tresholds = newTresholds;
+        }
+        
+        _values = new Dictionary<int, GameObject>();
+        for (int i = 0; i < Tresholds.Length; i++)
+        {
+            _values.Add(Tresholds[i], GameObjects[i]);
+        }
+    }
+
+    private void Start()
+    {
+        _health = GetComponent<Health>();
+    }
+
+    private void OnEnable()
+    {
+        foreach (var pair in _values)
+        {
+            pair.Value.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        foreach (var pair in _values)
+        {
+            if (_health.CurrentHealth < pair.Key && pair.Value != null && pair.Value.activeSelf)
+            {
+                var t = pair.Value.transform;
+                Vector3 position = t.position;
+                Quaternion rotation = t.rotation;
+                
+                GameObject destroyEffectObject = Instantiate(DestroyEffect.gameObject, position, rotation);
+                var main = destroyEffectObject.GetComponent<ParticleSystem>().main;
+                main.stopAction = ParticleSystemStopAction.Destroy;
+                
+                switch (DestructionMethod)
+                {
+                    case DestructionMethods.Destroy:
+                        Destroy(pair.Value);
+                        break;
+                    case DestructionMethods.Disable:
+                        pair.Value.SetActive(false);
+                        break;
+                }
+            }
+        }
+    }
+}
