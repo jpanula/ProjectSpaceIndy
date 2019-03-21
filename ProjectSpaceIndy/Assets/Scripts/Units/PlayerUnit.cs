ecd5596d4154bce4d2653fbbc37a0a48d28c2f86
Jaakko <panula.jaakko@gmail.com>
2019-03-21 17:12:37 +0200

    added a hud near the ship with fuel and hp with basic functionality

--- ProjectSpaceIndy/Assets/Scripts/Units/PlayerUnit.cs
+++ ProjectSpaceIndy/Assets/Scripts/Units/PlayerUnit.cs
@@ -10,16 +10,29 @@ public class PlayerUnit : UnitBase
 	[FormerlySerializedAs("FuelAmount")] public float _fuelAmount;
 	public float FuelCap;
 	public float ShootDelay;
+	
+	[Header("UI Stuff")]
 	public TextMeshProUGUI FuelText;
-
 	public Image HealthBar;
-	public float HealthBarChangeSpeed;
+	public float HealthBarChangeSpeed = 10;
+	public float FuelBarChangeSpeed = 10;
+	public float ShipHUDTimeout = 3;
+	public float ShipHUDFadeOutSpeed = 1;
+	public float ShipHUDFadeInSpeed = 1;
+	public Image[] ShipHUDElements = new Image[6];
+	
 	private Vector3 _spawnPosition;
 	private PlayerMover _playerMover;
 	private float _speed;
 	private float _boostSpeed;
 	private bool _speedValuesSaved;
 	private float _shootDelayTimer;
+
+	private Image _hpBarFill;
+	private Image _fuelBarFill;
+	private float[] _shipHUDAlpha;
+	private float _shipHUDTimeoutTimer;
+	private int _lastHP;
 
 	public float FuelAmount
 	{
@@ -30,16 +43,49 @@ public class PlayerUnit : UnitBase
 	private void Start()
 	{
 		_playerMover = GetComponent<PlayerMover>();
+		foreach (var image in ShipHUDElements)
+		{
+			if (image.name.Contains("HPBarFill"))
+			{
+				_hpBarFill = image;
+			}
+			else if (image.name.Contains("FuelBarFill"))
+			{
+				_fuelBarFill = image;
+			}
+		}
+		_shipHUDAlpha = new float[ShipHUDElements.Length];
+		for (int i = 0; i < ShipHUDElements.Length; i++)
+		{
+			_shipHUDAlpha[i] = ShipHUDElements[i].color.a;
+			
+			Image image = ShipHUDElements[i];
+			
+			if (image.name.Contains("HPBarFill"))
+			{
+				_hpBarFill = image;
+			}
+			else if (image.name.Contains("FuelBarFill"))
+			{
+				_fuelBarFill = image;
+			}		
+		}
+
+		_lastHP = Health.CurrentHealth;
 	}
 
 	protected override void Update ()
 	{
 		float fillAmount = (float) Health.CurrentHealth / Health.MaxHealth;
 		HealthBar.fillAmount = Mathf.Lerp(HealthBar.fillAmount, fillAmount, Time.deltaTime * HealthBarChangeSpeed);
+		_hpBarFill.fillAmount = HealthBar.fillAmount;
+		fillAmount = FuelAmount / FuelCap;
+		_fuelBarFill.fillAmount = Mathf.Lerp(_fuelBarFill.fillAmount, fillAmount, Time.deltaTime * FuelBarChangeSpeed);
 		FuelText.text = "Fuel: " + FuelAmount.ToString("0.00");
 		
 		if (Input.GetButton("Fire3") || Input.GetAxis("Triggers") != 0)
 		{
+			_shipHUDTimeoutTimer = 0;
 			if (!_speedValuesSaved)
 			{
 				_speed = _playerMover.Speed;
@@ -69,6 +115,39 @@ public class PlayerUnit : UnitBase
 			{
 				_playerMover.Speed = _speed;
 				_speedValuesSaved = false;
+			}
+		}
+
+		if (_lastHP != Health.CurrentHealth)
+		{
+			_lastHP = Health.CurrentHealth;
+			_shipHUDTimeoutTimer = 0;
+		}
+		
+		_shipHUDTimeoutTimer += Time.deltaTime;
+		if (_shipHUDTimeoutTimer >= ShipHUDTimeout)
+		{
+			foreach (var image in ShipHUDElements)
+			{
+				if (image.color.a > 0)
+				{
+					var imageColor = image.color;
+					imageColor.a = Mathf.Lerp(imageColor.a, 0, Time.deltaTime * ShipHUDFadeOutSpeed);
+					image.color = imageColor;
+				}
+			}
+		}
+		else
+		{
+			for (int i = 0; i < ShipHUDElements.Length; i++)
+			{
+				var image = ShipHUDElements[i];
+				if (image.color.a < _shipHUDAlpha[i])
+				{
+					var imageColor = image.color;
+					imageColor.a = Mathf.Lerp(imageColor.a, _shipHUDAlpha[i], Time.deltaTime * ShipHUDFadeInSpeed);
+					image.color = imageColor;
+				}
 			}
 		}
 	}
