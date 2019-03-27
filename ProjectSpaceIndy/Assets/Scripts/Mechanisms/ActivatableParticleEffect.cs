677768f5178ceb6c4b33594a604b72d2157f6c5c
Jaakko <panula.jaakko@gmail.com>
2019-03-27 14:47:38 +0200

    made an activatable mechanism that instantiates a particle effect when activated

---
+++ ProjectSpaceIndy/Assets/Scripts/Mechanisms/ActivatableParticleEffect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatableParticleEffect : MonoBehaviour
+{
+    public ActivatorBase[] Activators;
+    public ParticleSystem ParticleEffect;
+    public float ActivationTime;
+    protected bool Activated;
+    private float _activationTimer;
+
+    public virtual void Activate()
+    {
+        var t = transform;
+        Instantiate(ParticleEffect.gameObject, t.position, t.rotation);
+    }
+    
+    public virtual bool IsActivated()
+    {
+        foreach (var activator in Activators)
+        {
+            if (!activator.Active)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    protected virtual void Update()
+    {
+        if (IsActivated() && !Activated && _activationTimer >= ActivationTime)
+        {
+            Activate();
+            Activated = true;
+        }
+
+        _activationTimer += TimerManager.Instance.GameDeltaTime;
+    }
+}
