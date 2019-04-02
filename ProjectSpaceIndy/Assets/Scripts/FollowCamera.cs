74030aa5986d6821d031fb2d5736dcd44085c240
Jaakko <panula.jaakko@gmail.com>
2019-04-02 18:24:37 +0300

    made camera adjust according to movement and aiming
    rough version, making it nicer next

--- ProjectSpaceIndy/Assets/Scripts/FollowCamera.cs
+++ ProjectSpaceIndy/Assets/Scripts/FollowCamera.cs
@@ -11,6 +11,10 @@ public class FollowCamera : MonoBehaviour
 	[Range(0.0f, 90.0f), Tooltip("Angle at which the camera looks at the object")]
 	public float Angle;
 	public float MovementSmoothing;
+	[Tooltip("Changes how much aim and movement cause the camera to adjust")]
+	public float AdjustMultiplier;
+	[Tooltip("How many seconds should the camera adjustment stay before resetting")]
+	public float AdjustTimeout;
 	//private Vector3 defaultCamPos;
 	private Vector3 _newPos;
 
@@ -28,7 +32,30 @@ public class FollowCamera : MonoBehaviour
 		transform.eulerAngles = new Vector3(Angle, 0, 0);
 		if (target != null)
 		{
-			_newPos = target.transform.position +
+			Vector3 adjustmentVector = Vector3.zero;
+			
+			Vector3 rightStickVector = new Vector3(Input.GetAxisRaw("Horizontal_Look"), 0, Input.GetAxisRaw("Vertical_Look"));
+			if (rightStickVector.magnitude > 1)
+			{
+				rightStickVector = rightStickVector.normalized;
+			}
+			
+			Vector3 leftStickVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+			if (leftStickVector.magnitude > 1)
+			{
+				leftStickVector = leftStickVector.normalized;
+			}
+
+			if (rightStickVector.magnitude >= InputManager.Instance.RightStickDeadzone)
+			{
+				adjustmentVector = rightStickVector;
+			}
+			else if (leftStickVector.magnitude >= InputManager.Instance.LeftStickDeadzone)
+			{
+				adjustmentVector = leftStickVector;
+			}
+			
+			_newPos = target.transform.position + adjustmentVector * AdjustMultiplier + 
 			          Vector3.Normalize(new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * Angle),
 				          -Mathf.Cos(Mathf.Deg2Rad * Angle))) * Distance;
 			transform.position = Vector3.Lerp(transform.position, _newPos, 1 / MovementSmoothing * TimerManager.Instance.GameDeltaTime);
