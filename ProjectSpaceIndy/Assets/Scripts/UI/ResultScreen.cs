ab5c809bb20cc23169de299fa6e6dc0279ca06a3
Leia <laaksonenleia@gmail.com>
2019-04-02 17:12:51 +0300

    Started working on result screen

---
+++ ProjectSpaceIndy/Assets/Scripts/UI/ResultScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ResultScreen : MonoBehaviour
+{
+    public TextMeshProUGUI Score;
+    public TextMeshProUGUI Time;
+
+    private void OnEnable()
+    {
+        Score.text = GameManager.Score.ToString();
+        Time.text = TimerManager.Instance.ScaledGameTime.ToString();
+        TimerManager.Instance.SetGameTimeScale(0);
+    }
+}
