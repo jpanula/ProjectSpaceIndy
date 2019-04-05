ae02aea789cd05e58c0005398557f229988a34f7
Jaakko <panula.jaakko@gmail.com>
2019-04-05 15:32:50 +0300

    made camera adjustment work better

--- ProjectSpaceIndy/Assets/Scripts/FollowCamera.cs
+++ ProjectSpaceIndy/Assets/Scripts/FollowCamera.cs
@@ -14,10 +14,12 @@ public class FollowCamera : MonoBehaviour
 	public float AdjustSmoothing;
 	[Tooltip("Changes how much aim and movement cause the camera to adjust")]
 	public float AdjustMultiplier;
-	[Tooltip("How many seconds should the camera adjustment stay before resetting")]
-	public float AdjustTimeout;
 	[Tooltip("Lower values make the camera more springy when adjusting")]
 	public float Stiffness;
+	[Tooltip("How long should the player move before the camera starts adjusting to movement")]
+	public float MovementAdjustRelax;
+	[Tooltip("Holds a slowly falling deadzone for right stick values to prevent camera jumping back when moving stick back to neutral")]
+	public float HoldFor;
 	//private Vector3 defaultCamPos;
 	private Vector3 _newPos;
 	private float _adjustTimer;
@@ -25,89 +27,121 @@ public class FollowCamera : MonoBehaviour
 	private Vector3 _adjustmentVector;
 	private float _smoothing;
 	private float _adjustMultiplier;
-	private float _stiffValue;
+	private float _holdValue;
+	private float _movementRelaxTimer;
+	private float _returnTimer;
+	private bool _returning;
+	private PlayerUnit _player;
+	private Vector3 _startPosition;
+	private bool _returned;
 
 	// Use this for initialization
 	void Start ()
 	{
 		//defaultCamPos = transform.position;
 	}
-	
+
+	private void Awake()
+	{
+		//_player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerUnit>();
+		if (target == null)
+		{
+			target = GameObject.FindWithTag("Player");
+		}
+		_adjustMultiplier = AdjustMultiplier;
+	}
+
 	// Update is called once per frame
 	void Update ()
 	{
 		/*Vector3 newCamPos = target.transform.position + defaultCamPos;
 		transform.position = newCamPos;*/
-		_stiffValue -= TimerManager.Instance.GameDeltaTime / Stiffness;
-		_stiffValue = Mathf.Max(0, _stiffValue);
+		_holdValue -= TimerManager.Instance.GameDeltaTime / HoldFor;
+		_holdValue = Mathf.Max(0, _holdValue);
 		transform.eulerAngles = new Vector3(Angle, 0, 0);
-		if (target != null)
+			
+		Vector3 rightStickVector = new Vector3(Input.GetAxisRaw("Horizontal_Look"), 0, Input.GetAxisRaw("Vertical_Look"));
+		if (rightStickVector.magnitude > 1)
 		{
-			
-			Vector3 rightStickVector = new Vector3(Input.GetAxisRaw("Horizontal_Look"), 0, Input.GetAxisRaw("Vertical_Look"));
-			if (rightStickVector.magnitude > 1)
-			{
-				rightStickVector = rightStickVector.normalized;
-			}
-			
-			Vector3 leftStickVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-			if (leftStickVector.magnitude > 1)
-			{
-				leftStickVector = leftStickVector.normalized;
-			}
+			rightStickVector = rightStickVector.normalized;
+		}
+		
+		Vector3 leftStickVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		if (leftStickVector.magnitude > 1)
+		{
+			leftStickVector = leftStickVector.normalized;
+		}
 
-			if (rightStickVector.magnitude >= InputManager.Instance.RightStickDeadzone)
+		if (rightStickVector.magnitude >= InputManager.Instance.RightStickDeadzone)
+		{
+			if (rightStickVector.magnitude > _holdValue)
 			{
-				if (rightStickVector.magnitude > _stiffValue)
-				{
-					_stiffValue = rightStickVector.magnitude;
-				}
-				_adjustmentVector = rightStickVector.normalized * _stiffValue;
-				_adjusting = true;
+				_holdValue = rightStickVector.magnitude;
 			}
-			else if (leftStickVector.magnitude >= InputManager.Instance.LeftStickDeadzone)
+			_adjustmentVector = rightStickVector.normalized * _holdValue;
+			_movementRelaxTimer = 0;
+			_smoothing = AdjustSmoothing;
+			_adjusting = true;
+		}
+		else if (leftStickVector.magnitude >= InputManager.Instance.LeftStickDeadzone)
+		{
+			_movementRelaxTimer += TimerManager.Instance.GameDeltaTime;
+			if (_movementRelaxTimer >= MovementAdjustRelax)
 			{
+				_adjustmentVector = Vector3.zero;
+				_adjusting = false;
+
+				/*
 				if (leftStickVector.magnitude > _stiffValue)
 				{
 					_stiffValue = leftStickVector.magnitude;
 				}
+
 				_adjustmentVector = leftStickVector.normalized * _stiffValue;
 				_adjusting = true;
-				if (Input.GetAxisRaw("Triggers") != 0)
+				if (Input.GetAxisRaw("Triggers") != 0 && _player.FuelAmount > 0)
 				{
 					_adjustMultiplier = 2 * AdjustMultiplier;
 				}
 				else
 				{
 					_adjustMultiplier = AdjustMultiplier;
-				}
+				}*/
 			}
-			else
-			{
-				_adjusting = false;
-			}
-
-			if (_adjusting)
-			{
-				_adjustTimer = 0;
-				_smoothing = AdjustSmoothing;
-			}
-			else
-			{
-				_adjustTimer += TimerManager.Instance.GameDeltaTime;
-			}
-
-			if (_adjustTimer >= AdjustTimeout)
-			{
-				_adjustmentVector = Vector3.zero;
-				_smoothing = MovementSmoothing;
-			}
-				
-			_newPos = target.transform.position + _adjustmentVector * _adjustMultiplier + 
-			          Vector3.Normalize(new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * Angle),
-				          -Mathf.Cos(Mathf.Deg2Rad * Angle))) * Distance;
-			transform.position = Vector3.Lerp(transform.position, _newPos, 1 / _smoothing * TimerManager.Instance.GameDeltaTime);
 		}
 
+		if (_adjusting)
+		{
+			if (_smoothing < AdjustSmoothing)
+			{
+				_smoothing += TimerManager.Instance.GameDeltaTime / Stiffness;
+				_smoothing = Mathf.Min(_smoothing, AdjustSmoothing);
+			}
+			else if (_smoothing > AdjustSmoothing)
+			{
+				_smoothing -= TimerManager.Instance.GameDeltaTime / Stiffness;
+				_smoothing = Mathf.Max(_smoothing, AdjustSmoothing);
+			}
+		}
+		else
+		{
+			if (_smoothing > MovementSmoothing)
+			{
+				_smoothing -= TimerManager.Instance.GameDeltaTime / Stiffness;
+				_smoothing = Mathf.Max(_smoothing, MovementSmoothing);
+			}
+			else if (_smoothing < MovementSmoothing)
+			{
+				_smoothing += TimerManager.Instance.GameDeltaTime / Stiffness;
+				_smoothing = Mathf.Min(_smoothing, MovementSmoothing);
+			}
+		}
+
+			_newPos = target.transform.position + _adjustmentVector * _adjustMultiplier +
+			          Vector3.Normalize(new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * Angle),
+				          -Mathf.Cos(Mathf.Deg2Rad * Angle))) * Distance;
+
+			transform.position = Vector3.Lerp(transform.position, _newPos,
+				1 / _smoothing * TimerManager.Instance.GameDeltaTime);
 	}
 }
