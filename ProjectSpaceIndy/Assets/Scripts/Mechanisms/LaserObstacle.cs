a3facf7a43c4e208ea72def4473b91e8d3e62979
Leia <laaksonenleia@gmail.com>
2019-04-16 16:42:13 +0300

    Started working on a laser door in Level 2

---
+++ ProjectSpaceIndy/Assets/Scripts/Mechanisms/LaserObstacle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserObstacle : MonoBehaviour
+{
+    public ActivatorBase[] Activators;
+    [Tooltip("Gameobjects that will deactivate when the lasers turn off")]
+    public GameObject[] ObjectsToDeactivate;
+
+    private BoxCollider _boxCollider;
+
+    private void Start()
+    {
+        _boxCollider = this.GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        int counter = 0;
+
+        for (int i = 0; i < Activators.Length; i++)
+        {
+            if (Activators[i].Active)
+            {
+                counter++;
+            }
+        }
+
+        if (counter == Activators.Length)
+        {
+            DeactivateLasers();
+        }
+    }
+
+    private void DeactivateLasers()
+    {
+        foreach (GameObject gameObject in ObjectsToDeactivate)
+        {
+            gameObject.SetActive(false);
+        }
+        Destroy(_boxCollider);
+    }
+}
