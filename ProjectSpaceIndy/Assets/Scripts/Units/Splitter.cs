a5c9aee77f024931be8bed74589aec6f758341ec
Leia <laaksonenleia@gmail.com>
2019-04-23 13:35:44 +0300

    Added an invulnerability time to splitters

--- ProjectSpaceIndy/Assets/Scripts/Units/Splitter.cs
+++ ProjectSpaceIndy/Assets/Scripts/Units/Splitter.cs
@@ -6,14 +6,32 @@ public class Splitter : MonoBehaviour
 {
     public Animator Animator;
     private float _baseSpeed;
+    [SerializeField, Tooltip("How long the Splitter is invulnerable after spawn")]
+    private float _invulnerabilityTime;
+    private float _timer;
+    private bool _timerIsDone;
 
     private void Awake()
     {
         _baseSpeed = Animator.speed;
+        _timer = 0;
+        _timerIsDone = false;
+        GetComponent<Health>()._isInvulnerable = true;
     }
 
     private void Update()
     {
         Animator.speed = _baseSpeed * TimerManager.Instance.GameDeltaScale;
+
+        if (_timer >= _invulnerabilityTime && !_timerIsDone)
+        {
+            GetComponent<Health>()._isInvulnerable = false;
+            _timerIsDone = true;
+        }
+
+        if (!_timerIsDone)
+        {
+            _timer += Time.deltaTime * TimerManager.Instance.GameDeltaScale;
+        }
     }
 }
