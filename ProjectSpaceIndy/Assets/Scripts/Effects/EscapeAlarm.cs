2f1c24b032d6b8c38f5f250ea8aca7463ccaf350
Leia <laaksonenleia@gmail.com>
2019-05-14 13:46:12 +0300

    Added some sounds and "fixed" the spinning enemy sound
    It's even worse now send help

---
+++ ProjectSpaceIndy/Assets/Scripts/Effects/EscapeAlarm.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeAlarm : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    private bool _isAudioNull;
+    
+    void Start()
+    {
+        _isAudioNull = AudioSource == null;
+    }
+
+
+    void Update()
+    {
+        if (GameManager.EscapePhase && !_isAudioNull && !AudioSource.isPlaying)
+        {
+            AudioSource.Play();
+        }
+    }
+}
