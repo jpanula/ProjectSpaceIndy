ea182677ab92e026bda86815d12b2ba65fd0992a
Jaakko <panula.jaakko@gmail.com>
2019-05-21 20:38:58 +0300

    escape arrow implemented
    now just to add the paths to the stages

--- ProjectSpaceIndy/Assets/Scripts/UI/EscapeArrow.cs
+++ ProjectSpaceIndy/Assets/Scripts/UI/EscapeArrow.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class EscapeArrow : MonoBehaviour
 {
-    public float Speed;
+    public float Speed = 2000;
     public Path Path;
-    public float NextNodeDistance;
+    public float NextNodeDistance = 0.02f;
+    public float Margin = 0.02f;
+    public float RotationSpeed = 2;
     private RectTransform _rectTransform;
     private Camera _mainCam;
     private Node _currentNode;
     private Rect _viewportRect = new Rect(0, 0, 1, 1);
+    private Rect _screenRect;
 
     private void Awake()
     {
@@ -20,22 +23,39 @@ public class EscapeArrow : MonoBehaviour
 
     private void Update()
     {
+        _screenRect = new Rect(0 + Margin * Screen.width, 0 + Margin * Screen.height, Screen.width - Margin * 2 * Screen.width, Screen.height - Margin * 2 * Screen.height);
+        
         if (!_currentNode)
         {
             _currentNode = Path.GetClosestNode(_mainCam.ScreenToWorldPoint(_rectTransform.position));
         }
 
-        Debug.Log("Arrow Position\nScreen: " + _rectTransform.position + " World: " + _mainCam.ScreenToWorldPoint(_rectTransform.position));
-        Debug.Log("Node Position\nScreen: " + _mainCam.WorldToScreenPoint(_currentNode.GetPosition()) + " World: " + _currentNode.GetPosition());
-        if (_viewportRect.Contains(_mainCam.WorldToViewportPoint(_currentNode.GetPosition())))
-        {
-            _rectTransform.position = Vector3.Lerp(_rectTransform.position, _mainCam.WorldToScreenPoint(_currentNode.GetPosition()), TimerManager.Instance.UiDeltaTime * Speed / Vector3.Distance(_mainCam.ScreenToWorldPoint(_rectTransform.position), _currentNode.GetPosition()));
-        }
+        var nodeViewportPosition = _mainCam.WorldToViewportPoint(_currentNode.GetPosition());
+        nodeViewportPosition.z = 0;
 
-        if (Vector3.Distance(_mainCam.ScreenToWorldPoint(_rectTransform.position), _currentNode.GetPosition()) <=
-            NextNodeDistance)
+        var nodeScreenPosition = _mainCam.WorldToScreenPoint(_currentNode.GetPosition());
+        nodeScreenPosition.z = 0;
+
+        var arrowViewportPosition = _mainCam.ScreenToViewportPoint(_rectTransform.position);
+        
+        Debug.Log("Arrow Position\nScreen: " + _rectTransform.position + " World: " + _mainCam.ScreenToWorldPoint(_rectTransform.position));
+        Debug.Log("Node Position\nScreen: " + nodeScreenPosition + " World: " + _currentNode.GetPosition());
+        if (_viewportRect.Contains(_mainCam.WorldToViewportPoint(_currentNode.GetPosition())))
         {
             _currentNode = Path.GetNextNode(_currentNode);
         }
+
+        var direction = _mainCam.WorldToViewportPoint(_currentNode.GetPosition()) - new Vector3(0.5f, 0.5f, 0);
+        direction.z = 0;
+        var newRot = Quaternion.Euler(new Vector3(_rectTransform.eulerAngles.x, _rectTransform.eulerAngles.y, Mathf.Rad2Deg * Mathf.Atan2(direction.y, direction.x) + 90));
+        _rectTransform.rotation = Quaternion.Slerp(_rectTransform.rotation, newRot, TimerManager.Instance.UiDeltaTime * RotationSpeed);
+        if (direction.magnitude > 0.5 - Margin)
+        {
+            direction = direction.normalized / 2 * (1 - Margin);
+        }
+        direction = direction  + new Vector3(0.5f, 0.5f, 0);
+        var newPos = _mainCam.ViewportToScreenPoint(direction);
+        _rectTransform.position = Vector3.Lerp(_rectTransform.position, newPos, TimerManager.Instance.UiDeltaTime * Speed / Vector3.Distance(_rectTransform.position, newPos));
+
     }
 }
