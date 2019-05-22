e7e6d181699499bb53e7c17ec9bc768c0fd39605
Leia <l>
2019-05-22 18:40:17 +0300

    Added scrap pickup models and the arrow path to Level 2

--- ProjectSpaceIndy/Assets/Scripts/Pickups/ScrapPickup.cs
+++ ProjectSpaceIndy/Assets/Scripts/Pickups/ScrapPickup.cs
@@ -1,11 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ScrapPickup : PickupBase
 {
     public int Score;
-    
+    private MeshFilter _meshFilter;
+    public Mesh[] Models;
+    private Mesh _randomModel;
+
+    private void Awake()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+        if(Models.Length > 0)
+        {
+            int random = Random.Range(0, Models.Length);
+            _randomModel = Models[random];
+            if (_randomModel != null)
+            {
+                _meshFilter.mesh = _randomModel;
+            }
+        }
+    }
+
     protected override void GrantEffect(PlayerUnit playerUnit)
     {
         GameManager.Score += Score;
