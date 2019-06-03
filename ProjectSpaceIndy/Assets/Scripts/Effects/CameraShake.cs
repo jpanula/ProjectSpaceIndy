f07ee4c7a01fd391859f74d1feb2915e36412249
Leia <laaksonenleia@gmail.com>
2019-06-03 20:16:38 +0300

    Made a basic camera shake
    Spawns an explosion sound every time it shakes

---
+++ ProjectSpaceIndy/Assets/Scripts/Effects/CameraShake.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EZCameraShake;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public float MinTimeBetweenShakes;
+    public float MaxTimeBetweenShakes;
+    private float _randomBetweenMinMax;
+    private float _countDown;
+
+    public float ShakeIntensity;
+    public float ShakeRoughness;
+    public float ShakeFadeInTime;
+    public float ShakeFadeOutTime;
+
+    public GameObject RandomExplosion;
+
+    private void Start()
+    {
+        _countDown = 0;
+        _randomBetweenMinMax = 0;
+    }
+
+    void Update()
+    {
+        if (GameManager.EscapePhase)
+        {
+            if (_randomBetweenMinMax == 0)
+            {
+                _randomBetweenMinMax = Random.Range(MinTimeBetweenShakes, MaxTimeBetweenShakes);
+            }
+            _countDown += Time.deltaTime * TimerManager.Instance.GameDeltaScale;
+
+            if (_countDown >= _randomBetweenMinMax)
+            {
+                _countDown = 0;
+                _randomBetweenMinMax = 0;
+                Instantiate(RandomExplosion);
+                CameraShaker.Instance.ShakeOnce(ShakeIntensity, ShakeRoughness, ShakeFadeInTime, ShakeFadeOutTime);
+            }
+        }
+    }
+}
